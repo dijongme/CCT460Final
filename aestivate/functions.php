f9e0b36d9a30ad6b409c094ba598b927e4b32870
dijongme <megandijong@gmail.com>
2016-03-11 19:12:14 -0500

    made changes to functions folder

--- aestivate/functions.php
+++ aestivate/functions.php
@@ -107,6 +107,15 @@ function aestivate_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Search', 'aestivate' ),
+		'id'            => 'sidebar-search',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'aestivate_widgets_init' );
 
@@ -115,16 +124,32 @@ add_action( 'widgets_init', 'aestivate_widgets_init' );
  */
 function aestivate_scripts() {
 	wp_enqueue_style( 'aestivate-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Open+Sans');
+	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
 
 	wp_enqueue_script( 'aestivate-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'aestivate-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	
+	
+	
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'aestivate_scripts' );
+
+/**
+ * Sets new excerpt length
+ *
+ */
+function new_excerpt_length($length)
+{
+	return 20;
+}
+add_filter('excerpt_length', 'new_excerpt_length');
+
 
 /**
  * Implement the Custom Header feature.
