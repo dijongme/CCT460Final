a600566b525253ae38cc7f73d4303f6edf9d89db
Zhuoqiao <zhuoqiao.li@mail.utoronto.ca>
2016-03-12 16:44:38 -0500

    Set up the Loop. Comment Add. Delete the sidebar in homepage
    Add detail comments, then other editors can easy to distinguish with
    different code area.
    
    Set up the Wordpress loop and display it. 3 recent posts should be
    display per page.
    
    Delete the sidebar.

--- aestivate/homepage.php
+++ aestivate/homepage.php
@@ -5,69 +5,56 @@
 
  get_header();
 ?>
-
+	<!-- PRIMARY -->
 	<div id="primary" class="content-area">
+    	<!-- MAIN -->
 		<main id="main" class="site-main" role="main">
-
+		<!-- CONTENT SECTION -->
 		<div class="content-section">
 		<?php
-		if ( have_posts() ) :
+			if(have_posts( )) :
+				//Starts the loop
+				while(have_posts( )) :
+					//Iterates the post index
+					the_post( );
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+					//Prints out the content
+					the_content( );
+				endwhile;
 
-			<?php
+				the_posts_navigation( );
+			else :
+				get_template_part('template-parts/content', 'none');
 			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-		
+			?>
 			</div>
-		<br>
-		<!--new section-->
-<div class="news-section">
-<?php 
-	//to retrieve recent posts
-	$paged = (get_query_var("page")) ? get_query_var("page") : 1;
-	$wp_query = new WP_Query(array("posts_per_page" => 3, "paged" => $paged));
+			<!--// CONTENT SECTION -->
+			<br>
+			<!-- NEWS SECTION -->
+			<div class="news-section">
+			<?php 
+			//Retrieves recent posts
+		$paged = (get_query_var("page")) ? get_query_var("page") : 1;
+		$wp_query = new WP_Query(array("posts_per_page" => 3, "paged" => $paged));
 	
+	//WP loop to display recent posts
 	if ($wp_query->have_posts()) :
 		while($wp_query->have_posts()) :
 			//iterates the post index
 			$wp_query->the_post();
 ?>
-
-	<div class="recent-post"> 
-		<?php the_post_thumbnail("medium") ?>
-		<h3><a href="<?php the_permalink()?>"><?php the_title() ?> </a></h3>
-		<p> <?php the_excerpt() ?> </p>
+		<div class="recent-post"> 
+			<?php the_post_thumbnail("medium") ?>
+			<h3><a href="<?php the_permalink()?>"><?php the_title() ?> </a></h3>
+			<p> <?php the_excerpt() ?> </p>
 	</div>
-	
 	<?php
 		endwhile;
 	?>
-	<br>
-	<div class="prev-next-links">
+		<br>
+		<div class="prev-next-links">
 	<?php 
+		//Adds pagination links to enable users to navigate between posts
 		previous_posts_link("<i class='fa fa-caret-left fa-lg'></i>");
 		next_posts_link("<i class='fa fa-caret-right fa-lg'></i>", $wp_query->max-num_pages);
 	?>
@@ -75,11 +62,12 @@
 	<?php
 		endif;
 	?>
-
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+	</div>
+	<!--//NEWS SECTION -->
+	</main>
+	<!-- //MAIN -->
+</div>
+<!-- //PRIMARY -->
+<br>
 <?php
-get_sidebar();
-get_footer();
+get_footer( );
