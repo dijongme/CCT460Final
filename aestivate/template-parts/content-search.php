7893908a5794b4d8cea4e73f2abe6221458eaf24
sstianyi <scorpiotianyi@gmail.com>
2016-03-13 12:16:52 -0400

    changes made to content searching page
    -allows users to get all posts from a certain category when they
    command search
    -display the URL of each post when users command search
    -remove footer

--- aestivate/template-parts/content-search.php
+++ aestivate/template-parts/content-search.php
@@ -12,19 +12,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php aestivate_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+			<a href="<?php the_permalink( ) ?>"><?php the_permalink( ) ?></a>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php aestivate_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
