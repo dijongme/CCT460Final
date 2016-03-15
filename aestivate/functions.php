427f8568da236a09b9da2b420df70548f887b7cf
Zhuoqiao <zhuoqiao.li@mail.utoronto.ca>
2016-03-15 12:36:06 -0400

    comment add

--- aestivate/functions.php
+++ aestivate/functions.php
@@ -124,17 +124,15 @@ add_action( 'widgets_init', 'aestivate_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
-function aestivate_scripts() {
+function aestivate_scripts() 
+{
 	wp_enqueue_style( 'aestivate-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Open+Sans');
 	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
 
+	//Scripts for the menu button
 	wp_enqueue_script( 'aestivate-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'aestivate-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	
-	
-	
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
