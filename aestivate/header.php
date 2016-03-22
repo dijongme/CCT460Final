ff3068b4b1cc0b1ed7a666309eac05351a7976ec
dijongme <megandijong@gmail.com>
2016-03-21 23:38:07 -0400

    closed an open tag

--- aestivate/header.php
+++ aestivate/header.php
@@ -9,55 +9,63 @@
  * @package aestivate
  */
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+?>
+<!DOCTYPE html>
+<html <?php language_attributes( ); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
+<?php
+	wp_head( ); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class( ); ?>>
+<!-- PAGE -->
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'aestivate' ); ?></a>
+	<!-- BOX -->
+	<div class="box top-green-border">
+		<!-- MASTHEAD -->
+		<header id="masthead" class="site-header" role="banner">
+			<!-- SEARCH -->
+			<div id="search-widget">
+				<i class="fa fa-search"></i>
+				<?php dynamic_sidebar( 'sidebar-search' ); ?>
+			</div>
+			<!-- //SEARCH -->
+			<br>
+			<!-- SITE-BRANDING -->
+			<div class="site-branding">
+				<img id="logo" src="<?php print(wp_get_attachment_url(get_theme_mod("logo_setting"))) ?>" />
 
-<!--BOX-->
-<div class="box top-green-border">
+				<?php
+				if(is_front_page( ) && is_home( )) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
 
-<!--Masthead-->
-	<header id="masthead" class="site-header" role="banner">
-<!--Sidebar-search-->
-<div id="search-widget">
-<i class="fa fa-search"></i>
-<?php dynamic_sidebar( 'sidebar-search' ); ?>
-</div>
-<br>
-<!--site branding-->
-		<div class="site-branding">
-		<img id="logo" src="<?php print(wp_get_attachment_url(get_theme_mod("logo_setting"))) ?>" />	
-			
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+				$description = get_bloginfo( 'description', 'display' );
+				if($description || is_customize_preview( )) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div>
+			<!-- //SITE-BRANDING -->
+			<br>
+			<!-- SITE-NAVIGATION -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src = "<?php print(get_template_directory_uri()) ?>/img/hamburgericon.png"></button>
+                <br clear="both">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav>
+			<!-- //SITE-NAVIGATION -->
+		</header>
+		<!-- //MASTHEAD -->
+	
+		<!-- CONTENT -->
+		<div id="content" class="site-content">
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-<br>
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'aestivate' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
