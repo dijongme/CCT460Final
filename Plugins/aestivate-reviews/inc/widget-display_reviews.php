d3da54a11c3347f3841cc4ed3ed9fccc818f6110
sstianyi <scorpiotianyi@gmail.com>
2016-03-22 13:01:11 -0400

    set variables and retrieve saved values

--- Plugins/aestivate-reviews/inc/widget-display_reviews.php
+++ Plugins/aestivate-reviews/inc/widget-display_reviews.php
@@ -23,21 +23,20 @@ class Display_Reviews_Widget extends WP_Widget
 	 */
 	public function widget($args, $instance)
 	{
-		//Variables
-		$title = "Recent Reviews";
+		
 
 		print($args["before_widget"]);
 
 		//Prints title if not empty
-		if(!empty($title))
+		if(!empty($instance["title"]))
 		{
-			print($args["before_title"] . $title . $args["after_title"]);
+			print($args["before_title"] . apply_filters("widget_title", $instance["title"]) . $args["after_title"]);
 		}
 ?>
 		<ul>
 <?php
 		//Retrieves recent reviews
-		$wp_query = new WP_Query(array("post_type" => "reviews", "posts_per_page" => 3));
+		$wp_query = new WP_Query(array("post_type" => "reviews", "posts_per_page" => ((!empty($instance["Number of Posts to Display"])) ? $instance["Number of Posts to Display"] : 3), "order" => ((!empty($instance["order"])) ? $instance["order"] : "DESC")));
 
 		//WP loop to display recent reviews
 		if($wp_query->have_posts( ))
@@ -47,9 +46,10 @@ class Display_Reviews_Widget extends WP_Widget
 				//Iterates the post index
 				$wp_query->the_post( );
 ?>
-				<li>
-					<a href="<?php the_permalink( ) ?>"><?php the_title( ) ?></a>
-				</li>
+				
+					<?php the_post_thumbnail("thumbnail") ?><br>
+					<a href="<?php the_permalink( ) ?>"><?php the_title( ) ?></a><br>
+				
 <?php
 			}
 		}
@@ -58,6 +58,51 @@ class Display_Reviews_Widget extends WP_Widget
 <?php
 		print($args["after_widget"]);
 	}
+	
+	public function form($instance)
+	{ 
+		//Variables
+		$title 	= !empty($instance["title"]) ? $instance["title"] : __("Recent Reviews");
+		$number_of_posts_to_display = !empty($instance["Number of Posts to Display"]) ? $instance["Number of Posts to Display"] : __("3");
+		$order = !empty($instance["order"]) ? $instance["order"] : __("DESC");
+	
+	?>
+		<p>
+			<label for="<?php print($this->get_field_id("title"))?>"> <?php _e("title") ?>:</label>
+			<input id="<?php print($this->get_field_id("title"))?>" class="widefat" type="text" name="<?php print($this->get_field_name("title"))?>" value="<?php print(esc_attr($title))?>"/>
+		</p>
+		<p>		
+			<label for="<?php print($this->get_field_id("Number of Posts to Display"))?>"> <?php _e("Number of Posts to Display") ?>:</label>
+			<input id="<?php print($this->get_field_id("Number of Posts to Display"))?>" class="widefat" type="text" name="<?php print($this->get_field_name("Number of Posts to Display"))?>" value="<?php print(esc_attr($number_of_posts_to_display))?>"/>
+		</p>
+		<p>
+			<label for="<?php print($this->get_field_id("order"))?>"> <?php _e("order") ?>:</label>
+			<select id="<?php print($this->get_field_id("order"))?>" name="<?php print($this->get_field_name("order"))?>">
+				<option value="DESC" <?php (($order == "DESC") ? print("selected= 'selected'") : print("")) ?> >Descending</option>
+				<option value="ASC" <?php (($order == "ASC") ? print("selected= 'selected'") : print("")) ?> >Ascending</option>
+			</select>
+		</p>
+	
+	<?php	
+	}
+	
+	//updates the widget form
+		public function update($new_instance, $old_instance)
+	{
+		//Variables
+		$instance = array();
+
+		//Retrieves new saved values
+		$instance["title"]     = (!empty($new_instance["title"])) ? strip_tags($new_instance["title"]) : "Recent Reviews";
+		$instance["Number of Posts to Display"] = (!empty($new_instance["Number of Posts to Display"])) ? strip_tags($new_instance["Number of Posts to Display"]) : 3;
+		$instance["order"]     = (!empty($new_instance["order"])) ? strip_tags($new_instance["order"]) : "DESC";
+
+		//Returns instance
+		return $instance;
+	}
+
+
+
 }
 
 add_action("widgets_init", function( )
